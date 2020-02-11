ff8124b0fc4c81b03700f61883c6d8f5c977c8f3
James <mrjames8124@gmail.com>
2020-02-11 14:54:54 -0800

    finished happy numbers problem

---
+++ happyNumbers/script.js
@@ -0,0 +1,20 @@
+function happy(n) {
+  debugger;
+  while (n > 10) {
+    const arrayOfStringNums = n.toString().split('')
+    const numberArray = arrayOfStringNums.map(Number)
+    const squaredArr = numberArray.map(number => number * number)
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    const added = squaredArr.reduce(reducer)
+    if(added === 10){
+      n = 1;
+    }else{
+      n = added;
+    }
+  }
+  if (n === 1) {
+    return true
+  } else {
+    false
+  }
+}
