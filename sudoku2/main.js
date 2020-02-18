799213afd3c774b9f8431d619c5e08c982169966
James <mrjames8124@gmail.com>
2020-02-18 11:28:28 -0800

    completed sudoku check

--- sudoku2/main.js
+++ sudoku2/main.js
@@ -1,8 +1,15 @@
 function sudoku2(grid) {
   let found = false;
-
   let arrNum = 0;
   let index = 0;
+  let arrayLoop = 0;
+  let newIndex = 0;
+  let objToCheck = {};
+  let newArrayLoop = 0;
+  let lastNewIndex = 0;
+  let newObjToCheck1 = {};
+  let newObjToCheck2 = {};
+  let newObjToCheck3 = {};
   for (let i = 0; i < 81; i++) {
     if (parseInt(grid[arrNum][index])) {
       if (grid[arrNum][index] === grid[0][index] && arrNum == !0) {
@@ -39,12 +46,7 @@ function sudoku2(grid) {
     } else {
       index++;
     }
-  }
 
-  let arrayLoop = 0;
-  let newIndex = 0;
-  let objToCheck = {};
-  for (let i = 0; i < 81; i++) {
     if (objToCheck.hasOwnProperty(grid[arrayLoop][newIndex])) {
       return false;
     }
@@ -58,27 +60,22 @@ function sudoku2(grid) {
     } else {
       newIndex++
     }
-  }
 
-
-  let newArrayLoop = 0;
-  let lastNewIndex = 0;
-  let newObjToCheck1 = {};
-  let newObjToCheck2 = {};
-  let newObjToCheck3 = {};
-
-
-  for (let i = 0; i <= 81; i++) {
-    debugger;
-    if (newObjToCheck1.hasOwnProperty(grid[newArrayLoop][lastNewIndex]) || newObjToCheck2.hasOwnProperty(grid[newArrayLoop][lastNewIndex]) || newObjToCheck3.hasOwnProperty(grid[newArrayLoop][lastNewIndex])) {
-      return false;
-    }
     if (parseInt(grid[newArrayLoop][lastNewIndex])) {
       if (lastNewIndex < 3) {
+        if (newObjToCheck1.hasOwnProperty(grid[newArrayLoop][lastNewIndex])) {
+          return false
+        }
         newObjToCheck1[grid[newArrayLoop][lastNewIndex]] = grid[newArrayLoop][lastNewIndex]
       } else if (lastNewIndex >= 3 && lastNewIndex < 6) {
+        if (newObjToCheck2.hasOwnProperty(grid[newArrayLoop][lastNewIndex])) {
+          return false
+        }
         newObjToCheck2[grid[newArrayLoop][lastNewIndex]] = grid[newArrayLoop][lastNewIndex]
       } else {
+        if (newObjToCheck3.hasOwnProperty(grid[newArrayLoop][lastNewIndex])) {
+          return false
+        }
         newObjToCheck3[grid[newArrayLoop][lastNewIndex]] = grid[newArrayLoop][lastNewIndex]
       }
     }
@@ -88,15 +85,12 @@ function sudoku2(grid) {
     } else {
       lastNewIndex++
     }
-    if (i === 26 || i === 52) {
+    if (i === 26 || i === 53) {
       newObjToCheck1 = {};
       newObjToCheck2 = {};
       newObjToCheck3 = {};
     }
   }
-
-
-
 
   if (found) {
     return false;
@@ -104,13 +98,3 @@ function sudoku2(grid) {
     return true;
   }
 }
-console.log(sudoku2(
-  [[".", "4", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", "4", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", "1", ".", ".", "7", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", "3", ".", ".", ".", "6", "."],
-    [".", ".", ".", ".", ".", "6", ".", "9", "."],
-    [".", ".", ".", ".", "1", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", "2", ".", "."],
-    [".", ".", ".", "8", ".", ".", ".", ".", "."]]))
