18d40a63fdc341e34575e25eeb9ed4ab76131d38
James <mrjames8124@gmail.com>
2020-02-18 19:10:10 -0800

    finished the shell of strstr intervie question

---
+++ strstr/main.js
@@ -0,0 +1,16 @@
+function strstr(s, x) {
+  debugger;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === x[0]) {
+      let newSlice = s.slice(i, (i + s.length));
+      if (newSlice === x) {
+        return i;
+      }
+    }
+  }
+
+
+  return -1;
+}
+
+console.log(strstr("CodefightsIsAwesome", "ISA"))
