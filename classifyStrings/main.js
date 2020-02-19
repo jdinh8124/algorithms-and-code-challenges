814938218ca698ee527a9800cb2a9f75c4f9ff26
James <mrjames8124@gmail.com>
2020-02-19 13:22:41 -0800

    finished containsDuplicates challenge

--- classifyStrings/main.js
+++ classifyStrings/main.js
@@ -1,32 +1,3 @@
-// function classifyStrings(s) {
-//   let consectVowels = 0;
-//   let consectConsts = 0;
-//   let wildCard = 0;
-//   debugger;
-//   for (let i = 0; i < s.length; i++) {
-//     if (s[i] === "a" || s[i] === "e" || s[i] === "i" || s[i] === "o" || s[i] === "u") {
-//       consectVowels++;
-//       consectConsts = 0;
-//     } else if (s[i] === "?") {
-//       wildCard++
-//     } else {
-//       consectConsts++;
-//       consectVowels = 0;
-//     }
-
-//     if (consectConsts >= 5 || consectVowels >= 3) {
-//       return "bad";
-//     } else if (consectConsts >= 4 && wildCard > 0 || wildCard === 3) {
-//       return "mixed"
-//     } else if (consectVowels >= 2 && wildCard > 0) {
-//       return "mixed"
-//     }
-//   }
-//   return "good";
-// }
-
-
-
 function classifyStrings(s) {
   let consectVowels = 0;
   let consectConsts = 0;
@@ -71,9 +42,7 @@ function classifyStrings(s) {
       }
     }
   }
-  if (newString === "sdf?r") {
-    return "bad";
-  }
+
   for (let i = 0; i < newString.length; i++) {
     if (newString[i] === "a" || newString[i] === "e" || newString[i] === "i" || newString[i] === "o" || newString[i] === "u") {
       consectVowels++;
