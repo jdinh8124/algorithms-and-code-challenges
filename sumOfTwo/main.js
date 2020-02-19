051504582963834addb3b0e598cdef6fdae717a8
James <mrjames8124@gmail.com>
2020-02-19 15:37:31 -0800

    completed but need to optimize code to pass challenge

--- sumOfTwo/main.js
+++ sumOfTwo/main.js
@@ -1,20 +1,17 @@
 function sumOfTwo(a, b, v) {
-
-  let newObj = {};
+  const arrToCheck = [];
   for (let i = 0; i < a.length; i++) {
-    newObj[a[i]] = a[i];
-  }
-
-  for (let i = 0; i < b.length; i++) {
-    for (let key in newObj) {
-      if ((newObj[key] + b[i]) === v) {
-        return true;
-      }
+    for (let w = 0; w < b.length; w++) {
+      arrToCheck.push(a[i] + b[w]);
     }
-
   }
 
+
+  if (arrToCheck.includes(v)) {
+    return true
+  }
   return false;
 }
+
 
 console.log(sumOfTwo([1, 2, 3], [10, 20, 30, 40], 42))
