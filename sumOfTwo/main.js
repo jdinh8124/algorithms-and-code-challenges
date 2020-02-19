2cf6eb6eed02fd3d376ccc20669c55110653e98b
James <mrjames8124@gmail.com>
2020-02-19 15:46:57 -0800

    finished interview code challenge and code speed test

--- sumOfTwo/main.js
+++ sumOfTwo/main.js
@@ -1,17 +1,16 @@
 function sumOfTwo(a, b, v) {
-  const arrToCheck = [];
+  const objToCheck = {};
   for (let i = 0; i < a.length; i++) {
-    for (let w = 0; w < b.length; w++) {
-      arrToCheck.push(a[i] + b[w]);
-    }
+    objToCheck[v - a[i]] = [v - a[i]];
   }
 
-
-  if (arrToCheck.includes(v)) {
-    return true
+  for (let i = 0; i < b.length; i++) {
+    if (objToCheck.hasOwnProperty(b[i])) {
+      return true
+    }
   }
   return false;
 }
 
 
-console.log(sumOfTwo([1, 2, 3], [10, 20, 30, 40], 42))
+//Optimize to pass interview speed checkers by not nesting for loops
