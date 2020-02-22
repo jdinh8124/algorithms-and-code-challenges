3cfc5b0523fb7733af63943623d6b3bdeb342d50
James <mrjames8124@gmail.com>
2020-02-22 14:09:30 -0800

    passed all tests failling same as previous pattern test

--- fitThePattern/main.js
+++ fitThePattern/main.js
@@ -3,11 +3,14 @@ function checkPattern(arr, pattern) {
   debugger;
   const objToCheck ={};
     for(let i = 0 ; i < arr.length; i++){
+      if(i !== 0 && arr[i] === arr[i-1]){
+        return false;
+      }
       if(!objToCheck[pattern[i]]){
         objToCheck[pattern[i]] = arr[i]
       }else{
         for(let w = 0; w < arr[i].length; w++ ){
-          if (arr[i][w] !== objToCheck[pattern[i][w]]){
+          if (arr[i][w] !== objToCheck[pattern[i]][w]){
             return false
           }
         }
@@ -16,4 +19,4 @@ function checkPattern(arr, pattern) {
     return true;
 }
 
-console.log(checkPattern([[1, 1], [2, 2], [1, 1], [2, 2]], "ABAB"))
+console.log(checkPattern([[1, 2], [1, 2], [1, 2], [1, 2]], "ABAB"))
