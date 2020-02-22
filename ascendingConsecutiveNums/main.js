306523898342dce9c09cc90aeee04400e7d92d04
James <mrjames8124@gmail.com>
2020-02-21 19:05:12 -0800

    working through this challenge

--- ascendingConsecutiveNums/main.js
+++ ascendingConsecutiveNums/main.js
@@ -6,24 +6,40 @@ function ascending(str) {
   const nums = splitArr.map(nums => parseInt(nums));
   const newArrJoined = [];
   let stringToPush = "";
+  let numToPush;
   debugger;
 
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === 0 || stringToPush === "" || typeof nums[i - 1] === "undefined" || newArrJoined[0].length !== stringToPush.length) {
+    if (nums[i] === 0 || stringToPush === "" || typeof nums[i - 1] === "undefined" || nums[i] > nums[i-1] ) {
       if (typeof nums[i - 1] === "undefined" || typeof newArrJoined[0] === "undefined" || newArrJoined[0].length > 0 ){
-        if ( typeof newArrJoined[0] === "undefined" ||newArrJoined[0].length !== stringToPush.length){
+        if ( typeof newArrJoined[0] === "undefined" || numToPush !== stringToPush.length){
         stringToPush += nums[i];
       }else{
           newArrJoined.push(stringToPush);
+          numToPush = newArrJoined[0].length;
           stringToPush = "";
           stringToPush += nums[i];
       }
     }
     } else if(stringToPush.length > 0) {
-      newArrJoined.push(stringToPush);
-      stringToPush = "";
-      stringToPush += nums[i];
+      if(!numToPush){
+        newArrJoined.push(stringToPush);
+        numToPush = newArrJoined[0].length;
+        stringToPush = "";
+        stringToPush += nums[i];
+      }else {
+        if (numToPush === stringToPush.length) {
+          newArrJoined.push(stringToPush);
+          numToPush = newArrJoined[0].length;
+          stringToPush = "";
+          stringToPush += nums[i];
+        } else {
+          stringToPush += nums[i];
+
+        }
+      }
     }
+
   }
 
   for (let i = 1; i < newArrJoined.length; i++) {
@@ -41,4 +57,4 @@ function ascending(str) {
   return false;
 }
 
-console.log(ascending("57585960616263"))
+console.log(ascending("1234567"))
