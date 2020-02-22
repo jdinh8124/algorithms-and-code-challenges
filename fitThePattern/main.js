1a5a01fcbdb7fab60d22cef7023a5e0a6b6dc766
James <mrjames8124@gmail.com>
2020-02-22 14:23:24 -0800

    finished equation and checkes if the numbers stay the same

--- fitThePattern/main.js
+++ fitThePattern/main.js
@@ -1,13 +1,20 @@
 
 function checkPattern(arr, pattern) {
-  debugger;
   const objToCheck ={};
+  let same = false;
     for(let i = 0 ; i < arr.length; i++){
-      if(i !== 0 && arr[i] === arr[i-1]){
-        return false;
-      }
+
       if(!objToCheck[pattern[i]]){
         objToCheck[pattern[i]] = arr[i]
+        for (let x = 0; x < arr[i].length; x++) {
+          if(pattern[i] !== pattern[i-1]){
+            if (i !== 0 && arr[i][x] === arr[i -1][x]){
+              same = true;
+            }else{
+              same = false;
+            }
+          }
+        }
       }else{
         for(let w = 0; w < arr[i].length; w++ ){
           if (arr[i][w] !== objToCheck[pattern[i]][w]){
@@ -15,6 +22,9 @@ function checkPattern(arr, pattern) {
           }
         }
       }
+    }
+    if(same){
+      return false;
     }
     return true;
 }
