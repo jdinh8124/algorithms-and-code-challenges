ae5447b691dbe107d73c71786e245479e2a8cd46
James <mrjames8124@gmail.com>
2020-02-23 21:47:47 -0800

    finishing checking for duplicates

--- compentitionRank/main.js
+++ compentitionRank/main.js
@@ -9,19 +9,28 @@ debugger;
   console.log(arrToCompare)
 
   let num = 1;
-  let same = true;
   for(let i = 0; i < arrToCompare.length; i++){
   if(i === 0){
     arrOfFinish.push(num);
-    num++;
     return;
   }
-  // if(){
+  if(arrToCompare[i][0] === arrToCompare[0][0]){
+    arrOfFinish.push(num);
+    return;
+  }
 
-  // }
+    if (arrToCompare[i][0] === arrToCompare[i+1][0]){
+      let duplicate = 0;
+      for (let w = i + 1; w < (arrToCompare.length - i); w++){
+        if (arrToCompare[i][0] === arrToCompare[w][0]){
+
+        }
+      }
+    }  else{
+    num++;
     arrOfFinish.push(num);
   }
 }
-
+}
 
 console.log(competitionRank(competitionRank({ Aria: 90, Brooke: 90, Olivia: 90, Eve: 74, Ellie: 87 }, "Ellie"), 4))
