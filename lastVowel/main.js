8ca9e0970f9c0963405dea5dd70e79a58986500a
James <mrjames8124@gmail.com>
2020-02-25 09:22:21 -0800

    finished lastVowel function

--- lastVowel/main.js
+++ lastVowel/main.js
@@ -1,21 +1,37 @@
 function removeLastVowel(str) {
   const arrToReturn = [];
   const newArr = str.split(" ");
-  debugger;
   let newString;
+  let foundArray =[];
+  let wordToCheck;
    for(let i = 0; i < newArr.length; i++){
-     let numberToRemove = newArr[i].indexOf("a" || "e" || "i" || "o" || "u" || "A" || "E" || "I" || "O" || "U");
+     foundArray = []
+     wordToCheck = newArr[i].split("").reverse().join("")
+      foundArray.push(wordToCheck.toLowerCase().indexOf("a"));
+      foundArray.push(wordToCheck.toLowerCase().indexOf("e"));
+      foundArray.push(wordToCheck.toLowerCase().indexOf("i"));
+      foundArray.push(wordToCheck.toLowerCase().indexOf("o"));
+      foundArray.push(wordToCheck.toLowerCase().indexOf("u"));
+      let numberToRemove = null;
+      foundArray.sort()
+        for(let i = 0; i < foundArray.length; i++){
+          if(numberToRemove === null || numberToRemove === -1){
+            numberToRemove = foundArray[i]
+          }
+        }
      if(numberToRemove === 0){
-        newString = newArr[i].slice(numberToRemove);
+        newString = wordToCheck.slice(numberToRemove + 1);
+       arrToReturn.push(newString.split("").reverse().join(""))
      }else if (numberToRemove === -1){
-       arrToReturn.push(newArr[i]);
+       arrToReturn.push(wordToCheck.split("").reverse().join(""));
      }
      else{
-       newString = newArr[i].slice(0, numberToRemove - 1) + newArr[i].slice(numberToRemove);
+       newString = wordToCheck.slice(0, numberToRemove) + wordToCheck.slice(numberToRemove + 1);
+       arrToReturn.push(newString.split("").reverse().join(""))
      }
-    arrToReturn.push(newString)
     }
+    return arrToReturn.join(" ");
 }
 
 
-console.log(removeLastVowel("Those who dare to fail miserably can achieve greatly."))
+console.log(removeLastVowel("If you want to live a happy life, tie it to a goal, not to people."))
