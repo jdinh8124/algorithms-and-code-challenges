31af8fb4a1c01f65b4e963b76b12cfc851d44a1a
James <mrjames8124@gmail.com>
2020-02-26 21:46:35 -0800

    finished structure of assignment

---
+++ hasPathWithGivenSum/main.js
@@ -0,0 +1,10 @@
+//
+// Binary trees are already defined with this interface:
+// function Tree(x) {
+//   this.value = x;
+//   this.left = null;
+//   this.right = null;
+// }
+function hasPathWithGivenSum(t, s) {
+
+}
