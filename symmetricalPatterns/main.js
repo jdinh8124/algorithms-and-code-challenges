44d62b5f19bc74c20a9705efcb6b3eff2596728d
James <mrjames8124@gmail.com>
2020-02-27 19:12:30 -0800

    finished the structure of code using for loops

--- symmetricalPatterns/main.js
+++ symmetricalPatterns/main.js
@@ -1,3 +1,29 @@
 function classifyRug(pattern) {
+  let notImperfect = false;
+  let notHoriz = false;
+  let notVertSym = false;
+  let notPerf = false;
 
+
+  for(let i = 0; i < pattern[i].length ;i++){
+
+    for(let w = 0; w < pattern.length ; w++){
+
+    }
+
+  }
+
+
+
+  if(notVertSym && !notHoriz){
+    return "horizontally symmetric";
+  }
+  if (!notVertSym && notHoriz){
+    return "vertically symmetric";
+  }
+  if (!notVertSym && !notHoriz) {
+    return "imperfect";
+  }
+
+  return "perfect";
 }
