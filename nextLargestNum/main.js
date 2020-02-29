c038e57956fb067eea4ec783ef9b614af7f9fe0b
James <mrjames8124@gmail.com>
2020-02-29 12:13:21 -0800

    considered recurrisive approach to creating all possible combinations

--- nextLargestNum/main.js
+++ nextLargestNum/main.js
@@ -1,12 +1,42 @@
 function nextNumber(num) {
-  let numberStringed =  num.toString();
-  const arrayOfNums = numberStringed.split("")
-  const numberedArr = arrayOfNums.map(Number);
-  numberedArr.sort();
-
-  let numString =""
-  for(let i = numberedArr.length -1; i > 0; i--){
-    numString += numberedArr[i];
+  const arrAllOpt = [];
+  let numberStringed = num.toString();
+  const numberedArr = numberStringed.split("")
+  for(let i = 0; i < numberedArr.length; i++ ){
+    let numToPush = numberedArr[i];
+    for(let w = 0; w < numberedArr.length; w++){
+      if(w !== i){
+        numToPush += numberedArr[w];
+      }
+    }
+    arrAllOpt.push(numToPush);
   }
-  return parseInt(numString);
+  debugger;
+  //   const numberedArr = arrayOfNums.map(Number);
+  arrAllOpt.sort();
+  let found = arrAllOpt.indexOf(item => item === num);
+  if(found === arrAllOpt.length - 1){
+    return num;
+  }else{
+    return arrAllOpt[found + 1];
+  }
 }
+
+console.log(nextNumber(899))
+
+
+
+
+
+// if (num.length === 2) {
+//   let numberStringed = num.toString();
+//   const arrayOfNums = numberStringed.split("")
+//   const numberedArr = arrayOfNums.map(Number);
+//   numberedArr.sort();
+
+//   let numString = ""
+//   for (let i = numberedArr.length - 1; i > 0; i--) {
+//     numString += numberedArr[i];
+//   }
+//   return parseInt(numString);
+// }
