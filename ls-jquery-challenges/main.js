0098738b70503bd3a0fa07b6f26bdea97f1b47fe
James <mrjames8124@gmail.com>
2020-03-11 16:26:47 -0700

    reversed array of items completed

--- ls-jquery-challenges/main.js
+++ ls-jquery-challenges/main.js
@@ -2,3 +2,22 @@
 $(".add-product-to-cart").insertBefore("#product-detail-page-vue")
 
 $("h4:contains(Arizona)").css("color", "red")
+
+$("img.img-responsive[alt='Bravado Amber']").not(".img-responsive.mCS_img_loaded").trigger("click")
+
+
+
+$("#thisCollection .product-element").toArray().reverse().each(function () {
+  $(this).appendTo('#thisCollection.row');
+});
+
+
+$($("#thisCollection .product-element").toArray().reverse()).each(function () {
+  $(this).appendTo('#thisCollection.row');
+});
+
+
+
+$($("#thisCollection .product-element").toArray().reverse()).each(function () {
+  $(this).appendTo('#thisCollection .row');
+});
