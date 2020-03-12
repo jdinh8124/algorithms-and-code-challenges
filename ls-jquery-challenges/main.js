24be1b4def2926a6865fccdec6085772d0e15577
James <mrjames8124@gmail.com>
2020-03-11 17:12:58 -0700

    finished magic stop

--- ls-jquery-challenges/main.js
+++ ls-jquery-challenges/main.js
@@ -1,13 +1,23 @@
 
+//jQuery Basics
+
+//1
 $(".add-product-to-cart").insertBefore("#product-detail-page-vue")
 
+//2
 $("h4:contains(Arizona)").css("color", "red")
 
+//3
 $("img.img-responsive[alt='Bravado Amber']").not(".img-responsive.mCS_img_loaded").trigger("click")
 
-
-
-
+//4
 $($("#thisCollection .product-element").toArray().reverse()).each(function () {
   $(this).appendTo('#thisCollection .row');
 });
+
+//Bonus
+
+//Remove John Image
+$("#John").remove()
+//stop "Magic"
+clearInterval(JohnMagic)
