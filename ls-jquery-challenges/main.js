e91663f721cb63c4dac88f08731433a7f6debd85
James <mrjames8124@gmail.com>
2020-03-13 15:19:22 -0700

    moved to below the jquery cdn

--- ls-jquery-challenges/main.js
+++ ls-jquery-challenges/main.js
@@ -19,5 +19,7 @@ $($("#thisCollection .product-element").toArray().reverse()).each(function () {
 
 //Remove John Image
 // $("#John").remove()
+
+$(".container > script:nth-child(1)").insertAfter("[src='https://code.jquery.com/jquery-3.4.1.min.js']")
 //stop "Magic"
 clearInterval(JohnMagic)
