03f9d5dbce3a5ef148ad77331d873c13ff33e8c7
James <mrjames8124@gmail.com>
2020-03-23 16:14:34 -0700

    removed console log

--- reverseOdd/main.js
+++ reverseOdd/main.js
@@ -1,14 +1,11 @@
 function reverseOdd(str) {
   let arr = str.split(" ")
   let reverseArr = arr.map(item => {
-    if(item.length % 2 == 0 ){
+    if (item.length % 2 == 0) {
       return item;
-    }else{
+    } else {
       return item.split("").reverse().join("")
     }
   })
-  return reverseArr;
+  return reverseArr.join(" ");
 }
-
-
-console.log(reverseOdd("One two three four") )
