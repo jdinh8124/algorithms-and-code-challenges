877e9ef82b79cb333af64fcf5e9557ed0afd69d0
James <mrjames8124@gmail.com>
2020-03-23 16:17:11 -0700

    shortened code

--- reverseOdd/main.js
+++ reverseOdd/main.js
@@ -1,11 +1,11 @@
 function reverseOdd(str) {
   let arr = str.split(" ")
   let reverseArr = arr.map(item => {
-    if (item.length % 2 == 0) {
-      return item;
-    } else {
+    if (item.length % 2 != 0) {
       return item.split("").reverse().join("")
     }
+    return item
   })
   return reverseArr.join(" ");
 }
+console.log(reverseOdd("One two three four"))
