8cf67475258afc3ef071825403e650952760a9b6
James <mrjames8124@gmail.com>
2020-03-24 15:08:09 -0700

    working through negative issues

--- evalAlgebra/main.js
+++ evalAlgebra/main.js
@@ -4,26 +4,26 @@ function evalAlgebra(eq) {
   let findIndex = arrEq.findIndex(index => index === "x");
   if(findIndex === 4){
       if(arrEq[1] === "+"){
-        return Math.abs(parseInt(arrEq[0])) + Math.abs(parseInt(arrEq[2]))
+        return parseInt(arrEq[0]) + Math.abs(parseInt(arrEq[2]))
       }else{
-        return Math.abs(parseInt(arrEq[0])) - Math.abs(parseInt(arrEq[2]))
+        return parseInt(arrEq[0]) - Math.abs(parseInt(arrEq[2]))
       }
   }else{
     if(arrEq[0] === "x"){
       if (arrEq[1] === "+") {
-        return Math.abs(parseInt(arrEq[4])) - Math.abs(parseInt(arrEq[2]))
+        return parseInt(arrEq[4]) - Math.abs(parseInt(arrEq[2]))
       } else {
-        return Math.abs(parseInt(arrEq[2])) - Math.abs(parseInt(arrEq[4]))
+        return Math.abs(parseInt(arrEq[2])) - parseInt(arrEq[4])
       }
     }else{
       if (arrEq[1] === "+") {
-        return Math.abs(parseInt(arrEq[4])) - Math.abs(parseInt(arrEq[0]))
+        return parseInt(arrEq[4]) - parseInt(arrEq[0])
       } else {
-        return Math.abs(parseInt(arrEq[0])) - Math.abs(parseInt(arrEq[4]))
+        return parseInt(arrEq[0]) - parseInt(arrEq[4])
       }
     }
   }
 }
 
 
-console.log(evalAlgebra("-29 - x = -170"))
+console.log(evalAlgebra("x - 46 = -2"))
