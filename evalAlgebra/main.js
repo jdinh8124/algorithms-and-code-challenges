d7d11df40d793800875f7c6dd36ec4687044fab2
James <mrjames8124@gmail.com>
2020-03-24 14:34:16 -0700

    working with debugger to solve issues with function

---
+++ evalAlgebra/main.js
@@ -0,0 +1,21 @@
+function evalAlgebra(eq) {
+  debugger;
+  const arrEq = eq.split(" ");
+  let findIndex = arrEq.findIndex(index => index === "x");
+  if(findIndex === 4){
+      if(arrEq[1] === "+"){
+        return parseInt(arrEq[0]) + parseInt(arrEq[2])
+      }else{
+        return parseInt(arrEq[0]) - parseInt(arrEq[2])
+      }
+  }else{
+    if (arrEq[1] === "+") {
+      return parseInt(arrEq[4]) - parseInt(arrEq[0])
+    }else{
+      return parseInt(arrEq[0]) - parseInt(arrEq[4])
+    }
+  }
+}
+
+
+console.log(evalAlgebra("x + 10 = 53"))
