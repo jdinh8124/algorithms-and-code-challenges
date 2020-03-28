e7ae06dd4c077d87c49a19bf414e6d231713b784
James <mrjames8124@gmail.com>
2020-03-28 13:08:37 -0700

    finished function

--- stringCleaver/main.js
+++ stringCleaver/main.js
@@ -1,13 +1,15 @@
 function cleave(str, words) {
-  debugger
   let stringToReturn = "";
   for(let i = 0; i < str.length; i++){
     let letterPlusWord = str[i];
     let foundWord;
     let foundIndex;
     let foundWordIndex;
-    for(let w = i+1; w < str.length; w++){
-      letterPlusWord  += str[w];
+
+    for(let w = i; w < str.length; w++){
+      if(w !== i ){
+        letterPlusWord += str[w];
+      }
       let num = words.findIndex(word => word === letterPlusWord);
       if(num > -1){
         foundWord = letterPlusWord;
@@ -35,7 +37,7 @@ function cleave(str, words) {
 
 const words = ['a', 'after', 'all', 'an', 'and', 'are', 'as', 'by', 'continued', 'deadlines', 'doubly', 'fish', 'for', 'go', 'happen', 'happened', 'i', 'illusion', 'is', 'long', 'love', 'lunchtime', 'make', 'moment', 'noise', 'nothing', 'of', 'or', 'people', 'problem', 'second', 'so', 'summarize', 'summary', 'thanks', 'the', 'then', 'they', 'time', 'to', 'whooshing']
 
-console.log(cleave('solongandthanksforallthefish' , words))
+console.log(cleave('foramomentnothinghappenedthenafterasecondorsonothingcontinuedtohappen' , words))
 
 
 
