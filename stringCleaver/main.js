484983a0d7cb9ec143f3a8a3fa71dcef6ea0e09e
James <mrjames8124@gmail.com>
2020-03-28 13:09:24 -0700

    cleaned function of console logs

--- stringCleaver/main.js
+++ stringCleaver/main.js
@@ -31,29 +31,3 @@ function cleave(str, words) {
   }
   return stringToReturn
 }
-
-
-
-
-const words = ['a', 'after', 'all', 'an', 'and', 'are', 'as', 'by', 'continued', 'deadlines', 'doubly', 'fish', 'for', 'go', 'happen', 'happened', 'i', 'illusion', 'is', 'long', 'love', 'lunchtime', 'make', 'moment', 'noise', 'nothing', 'of', 'or', 'people', 'problem', 'second', 'so', 'summarize', 'summary', 'thanks', 'the', 'then', 'they', 'time', 'to', 'whooshing']
-
-console.log(cleave('foramomentnothinghappenedthenafterasecondorsonothingcontinuedtohappen' , words))
-
-
-
-// function cleave(str, words) {
-//   debugger;
-//   let stringToReturn = "";
-//   words.map(word => {
-//     let found = false;
-//     let num = str.search(word);
-//     if (num > -1) {
-//       if (!stringToReturn) {
-//         stringToReturn = word;
-//       } else {
-//         stringToReturn += " " + word;
-//       }
-//     }
-//   })
-//   return stringToReturn
-// }
