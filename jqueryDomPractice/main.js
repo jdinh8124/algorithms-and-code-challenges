a930a1c80004cde258862041244a643194ffeaba
James <mrjames8124@gmail.com>
2020-03-29 14:09:56 -0700

    reversed action buttons

--- jqueryDomPractice/main.js
+++ jqueryDomPractice/main.js
@@ -2,4 +2,13 @@ $($(".flex-row:nth-of-type(1) a").toArray().reverse()).each(function () { $(this
 $($(".flex-row").toArray().reverse()).each(function () { $(this).appendTo("#CategoryTiles") })
 $("#block267989").insertAfter("#block211544")
 
-$("#design_ideas_block div:nth-child(5)")
+$("#design_ideas_block div:nth-child(7)")
+
+$($(".action-buttons > *").toArray().reverse()).each(function(){
+  $(this).appendTo("#app-header div:last-of-child")
+})
+
+
+$($(".action-buttons > *").toArray().reverse()).each(function () {
+  $(this).appendTo(".action-buttons")
+})
