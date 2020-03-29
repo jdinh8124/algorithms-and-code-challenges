d22d13c9b01abb7aeab41169f137884497600d37
James <mrjames8124@gmail.com>
2020-03-29 13:53:49 -0700

    reversed rows on categories section

--- jqueryDomPractice/main.js
+++ jqueryDomPractice/main.js
@@ -1,1 +1,2 @@
 $($(".flex-row:nth-of-type(1) a").toArray().reverse()).each(function () { $(this).appendTo('.flex-row:nth-of-type(1)') })
+$($(".flex-row").toArray().reverse()).each(function () { $(this).appendTo("#CategoryTiles") })
