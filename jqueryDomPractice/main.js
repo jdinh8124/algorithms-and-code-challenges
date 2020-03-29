9ed0395483613a04e9fcbb3e29dafc22e5e2ad33
James <mrjames8124@gmail.com>
2020-03-29 14:01:54 -0700

    found behind the desing

--- jqueryDomPractice/main.js
+++ jqueryDomPractice/main.js
@@ -1,3 +1,5 @@
 $($(".flex-row:nth-of-type(1) a").toArray().reverse()).each(function () { $(this).appendTo('.flex-row:nth-of-type(1)') })
 $($(".flex-row").toArray().reverse()).each(function () { $(this).appendTo("#CategoryTiles") })
 $("#block267989").insertAfter("#block211544")
+
+$("#design_ideas_block div:nth-child(5)")
