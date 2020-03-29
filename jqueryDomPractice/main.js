1091276b9b7a461ffb3e4210a624481868e3fb5d
James <mrjames8124@gmail.com>
2020-03-29 13:55:28 -0700

    moved categories passed hero image

--- jqueryDomPractice/main.js
+++ jqueryDomPractice/main.js
@@ -1,2 +1,3 @@
 $($(".flex-row:nth-of-type(1) a").toArray().reverse()).each(function () { $(this).appendTo('.flex-row:nth-of-type(1)') })
 $($(".flex-row").toArray().reverse()).each(function () { $(this).appendTo("#CategoryTiles") })
+$("#block267989").insertAfter("#block211544")
