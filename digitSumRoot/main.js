76cb8f17580f11cf8f2bef4f2fb08826a440afe2
James <mrjames8124@gmail.com>
2020-03-31 12:01:13 -0700

    recurrsive function fixed

--- digitSumRoot/main.js
+++ digitSumRoot/main.js
@@ -1,3 +1,16 @@
 function rootDigit(n) {
+  let numToPush = 0;
+  let arr = n.toString().split("");
+  arr.forEach(num => {
+    numToPush += parseInt(num);
+  })
 
+  if(numToPush < 10){
+    return numToPush
+  }else{
+    return rootDigit(numToPush)
+  }
 }
+
+
+console.log(rootDigit(999888777));
