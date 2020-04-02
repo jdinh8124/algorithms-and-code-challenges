784e798c97c678055713d9eb441f76054e8dc780
James <mrjames8124@gmail.com>
2020-04-02 12:40:39 -0700

    shortened code for challenge

--- arrayOfMults/main.js
+++ arrayOfMults/main.js
@@ -1,11 +1,7 @@
 function arrayOfMultiples(num, length) {
   const arrToReturn = [];
-    for(let i = 0; i < length; i++){
-      if(i === 0){
-        arrToReturn.push(num)
-      }else{
-        arrToReturn.push(num * (i + 1))
+    for(let i = 1; i <= length; i++){
+        arrToReturn.push(num * i)
       }
-    }
     return arrToReturn;
 }
