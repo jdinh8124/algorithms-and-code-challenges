db1ddd1a3e5c881d4eb592fbf320e5a0c9692524
James <mrjames8124@gmail.com>
2020-04-01 19:39:43 -0700

    working through function with debugger

--- capitalChallenge/main.js
+++ capitalChallenge/main.js
@@ -1,3 +1,21 @@
 function selectLetters(s1, s2) {
+  debugger;
+  let stringToReturn = "";
+  let length;
+  if(s1.length > s2.length){
+    length = s2.length;
+  }else{
+    length = s1.length;
+  }
+  for(let i = 0; i < length; i++){
+    if(s1[i] === s1[i].toUpperCase()){
+      stringToReturn += s2[i]
+    } else if (s2[i] === s2[i].toUpperCase()){
+      stringToReturn += s1[i]
+    }
+  }
 
+  return stringToReturn;
 }
+
+console.log(selectLetters("1234567", "XxXxX"))
