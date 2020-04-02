9650e0119f37ed48b19562bff31de365020d2784
James <mrjames8124@gmail.com>
2020-04-02 12:37:45 -0700

    created function with for loop and conditional

--- arrayOfMults/main.js
+++ arrayOfMults/main.js
@@ -1,3 +1,11 @@
 function arrayOfMultiples(num, length) {
-
+  const arrToReturn = [];
+    for(let i = 0; i < length; i++){
+      if(i === 0){
+        arrToReturn.push(num)
+      }else{
+        arrToReturn.push(num * (i + 1))
+      }
+    }
+    return arrToReturn;
 }
