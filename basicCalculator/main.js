546b870a2d8c3b4701ace4e47bb5cc80fe97e7f2
James <mrjames8124@gmail.com>
2020-04-04 10:58:59 -0700

    completed problem without IDE

--- basicCalculator/main.js
+++ basicCalculator/main.js
@@ -1,16 +1,16 @@
 function calculator(num1, operator, num2) {
   if (num2 === 0) {
-    Return "Can't divide by 0!";
+    return "Can't divide by 0!";
   } else {
     switch (operator) {
-	Case “+”:
-    Return Num1 + num2;
-    Case: “-”:
-    Return num1 - num2;
-    Case: “*”:
-    Return num1 * num2;
-    Case: “/”
-    Return num1 / 2;
+      case '+':
+        return num1 + num2;
+      case '-':
+        return num1 - num2;
+      case '*':
+        return num1 * num2;
+      case '/':
+        return num1 / num2;
+    }
   }
-}
 }
