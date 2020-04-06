016ec54e092bbf10f84171f178ca6f6133e6136d
James <mrjames8124@gmail.com>
2020-04-06 10:57:50 -0700

    finished challenged without IDE

--- reverseNum/main.js
+++ reverseNum/main.js
@@ -1,3 +1,13 @@
 function rev(n) {
-
+  let negative = false;
+  if (n.toString()[0] === '-') {
+    negative = true;
+  }
+  let num = n.toString().split('').reverse().join('')
+  if (negative) {
+    let numArr = num.split('')
+    numArr.pop()
+    num = numArr.join('')
+  }
+  return num;
 }
