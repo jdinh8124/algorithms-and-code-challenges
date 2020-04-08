6c438c063a5a472ed459e236f5afecc935f27f87
James <mrjames8124@gmail.com>
2020-04-08 10:47:47 -0700

    initial test

--- houseRobber/main.js
+++ houseRobber/main.js
@@ -1,3 +1,20 @@
 function houseRobber(nums) {
+  let cash = 0;
+  for(let i = 0; i < nums.length; i++){
+    let highest = Math.max(...nums);
+    let found = nums.find(number => number === highest)
+    cash += found;
+    if(i === 0){
+      nums.splice(0, 2)
+      i-= 3;
+    }else if(i === nums.length -1){
+      nums.splice(i-1 , 2)
+      i -= 3;
 
+    }else{
+      nums.splice(i - 1, 3)
+      i -= 4;
+    }
+  }
+  return cash
 }
