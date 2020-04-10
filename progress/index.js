5c0e7c05c79e1cad23866ac8ae43386bb297c48f
James <mrjames8124@gmail.com>
2020-04-09 17:15:27 -0700

    completed problem on google docs without IDE

--- progress/index.js
+++ progress/index.js
@@ -1,3 +1,9 @@
-function progressDays(runs) {
-
+function progressDays(arr) {
+  let progress = 0;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > arr[i - 1]) {
+      progress++
+    }
+    return progress;
+  }
 }
