00993d7935c2399d96f73e2573f1e01e32ee6df2
James <mrjames8124@gmail.com>
2020-04-10 19:58:59 -0700

    pushing to obj

--- scramble/main.js
+++ scramble/main.js
@@ -2,4 +2,9 @@ function scrambled(word1, word2){
   if(word1.length !== word2.length){
     return false;
   }
+  const obj = {};
+  for(let i = 0; i < word1.length; i++){
+    obj[word1[i]] = obj[word1[i]] + 1 ||  1;
+    obj[word2[i]] = obj[word2[i]] - 1 ||  1;
+  }
 }
