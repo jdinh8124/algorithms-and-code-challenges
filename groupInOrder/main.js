84be5295d53bfd1bad5fc6b91ba926193fde9c1b
James <mrjames8124@gmail.com>
2020-04-13 16:03:45 -0700

    finished function removed -1 from conditional check

--- groupInOrder/main.js
+++ groupInOrder/main.js
@@ -1,4 +1,5 @@
 function group(arr, size) {
+  debugger;
   const arrToReturn = [];
   let numOfArrs = Math.ceil(arr.length / size);
   for(let i = 0; i < numOfArrs; i++){
@@ -6,13 +7,15 @@ function group(arr, size) {
   }
   let arrNumToPush = 0;
   for(let i = 0; i < arr.length; i++){
-    if(arrNumToPush === arrToReturn.length - 1){
+    if(arrNumToPush === arrToReturn.length){
       arrNumToPush = 0;
     }
     arrToReturn[arrNumToPush].push(arr[i])
     arrNumToPush++
   }
+  return arrToReturn;
 }
+
 
 
 console.log(group([1, 2, 3, 4, 5, 6, 7, 8], 2))
