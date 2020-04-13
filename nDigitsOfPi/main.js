04fc1584f8172e1452bd165f8deffec8d4fc75ce
James <mrjames8124@gmail.com>
2020-04-13 10:23:00 -0700

    added string to pi object:

--- nDigitsOfPi/main.js
+++ nDigitsOfPi/main.js
@@ -1,8 +1,12 @@
 function pi(n) {
+  debugger;
 let stringToReturn = "";
-for(let i; i < n; i++){
-  let pi = Math.PI;
+for(let i = 0; i < n; i++){
+  let pi = Math.PI + "";
   stringToReturn += pi[0]
 }
 return stringToReturn
 }
+
+
+console.log(pi(10))
