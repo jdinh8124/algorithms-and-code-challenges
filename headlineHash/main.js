db4af704058965c5b81cd06ad7b55043602b47dd
James <mrjames8124@gmail.com>
2020-04-14 13:19:40 -0700

    initial function completed

--- headlineHash/main.js
+++ headlineHash/main.js
@@ -1,3 +1,11 @@
 function getHashTags(str) {
-
+  let arrToReturn;
+  const splitarr = str.split().sort(function(a,b){
+    return b.length - a.lengths
+  }
+  )
+  for(let i = 0; i < 3; i++){
+    arrToReturn.push("#"+splitarr[i])
+  }
+  return arrToReturn;
 }
