44dfb75987d28ece84e0ac799356ea224a5a13d8
James <mrjames8124@gmail.com>
2020-04-14 13:25:38 -0700

    lowercased letters when hashed

--- headlineHash/main.js
+++ headlineHash/main.js
@@ -1,11 +1,13 @@
 function getHashTags(str) {
-  let arrToReturn;
-  const splitarr = str.split().sort(function(a,b){
-    return b.length - a.lengths
+  let arrToReturn = [];
+  const splitarr = str.split(" ").sort(function (a, b) {
+    return b.length - a.length
   }
   )
-  for(let i = 0; i < 3; i++){
-    arrToReturn.push("#"+splitarr[i])
+  for (let i = 0; i < 3; i++) {
+    if (spitarr.length > i) {
+      arrToReturn.push("#" + splitarr[i].toLowerCase())
+    }
   }
   return arrToReturn;
 }
