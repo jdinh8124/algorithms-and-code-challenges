80559db5c6fd2b187b52744ffa8179c7619064e6
James <mrjames8124@gmail.com>
2020-04-15 17:38:01 -0700

    completed function

--- majorityVote/main.js
+++ majorityVote/main.js
@@ -1,22 +1,24 @@
 function majorityVote(arr) {
-  debugger
+  if (arr.length === 1) {
+    return arr[0]
+  }
   arr.sort();
   let highest;
   let highestTimes = 0;
   let occurance;
   let occuranceTimes = 0;
-  for(let i = 0; i < arr.length; i++){
-    if(i === 0){
+  for (let i = 0; i < arr.length; i++) {
+    if (i === 0) {
       occuranceTimes++
       occurance = arr[i]
-    }else if (arr[i] === occurance){
+    } else if (arr[i] === occurance) {
       occuranceTimes++
-      if(i === arr.length - 1){
+      if (i === arr.length - 1) {
         highestTimes = occuranceTimes;
         highest = occurance;
       }
-    }else{
-      if (occuranceTimes > highestTimes){
+    } else {
+      if (occuranceTimes > highestTimes) {
         highestTimes = occuranceTimes;
         highest = occurance;
       }
@@ -24,13 +26,12 @@ function majorityVote(arr) {
       occuranceTimes = 1;
     }
   }
-  if(arr.length / 2 >= highestTimes){
+  if (arr.length / 2 >= highestTimes) {
     return null;
-  }else{
+  } else {
     return highest
   }
 }
-
 
 
 
