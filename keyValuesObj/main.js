aaa41e729136c29c41022e4b7b3b400ae2c1d0d2
James <mrjames8124@gmail.com>
2020-04-16 10:34:42 -0700

    completed code challenge without IDE

--- keyValuesObj/main.js
+++ keyValuesObj/main.js
@@ -1,3 +1,8 @@
-function objectToArray(obj) {
-
+function objectToArray(object) {
+  const arrToReturn = [];
+  for (let key in object) {
+    let arrToPush = [key, object[key]];
+    arrToReturn.push(arrToPush);
+  }
+  return arrToReturn;
 }
