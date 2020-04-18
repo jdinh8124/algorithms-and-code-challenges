1f3002f9d1e0155c83bea6fac8e6a7ce20b0faa8
James <mrjames8124@gmail.com>
2020-04-17 19:51:49 -0700

    working through inner loop

--- uniqueChar/main.js
+++ uniqueChar/main.js
@@ -1,3 +1,13 @@
 function filterUnique(arr) {
-
+  const arrToReturn = []
+  arr.map(item =>{
+    let unique = true;
+    for(let i = 0; i < item.length; i++){
+      let splitArr= item.split("")
+    }
+    if(unique){
+      arrToReturn.push(item);
+    }
+  })
+  retirm arrToReturn;
 }
