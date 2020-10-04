8d3852c020823051cf23c6125ae836c98a60fd5e
James Dinh <jdinh8124@gmail.com>
2020-10-04 12:25:34 -0700

    completed challenge accounting for extra storage memory space

--- palindrome/script.ts
+++ palindrome/script.ts
@@ -2,5 +2,19 @@ function isPalindrome(x: number): boolean {
     return x.toString() === x.toString().split('').reverse().join('');
     };
 
-
-    
+// This second function doesn't take up space storing into arrays 
+    // function isPalindrome(x: number): boolean {
+    //     if(x < 0) {
+    //         return false
+    //     }
+        
+    //     let reversed = 0, y = x
+        
+    //     while (y > 0) {
+    //         const last = y % 10;
+    //         reversed = (reversed * 10 ) + last;
+    //         y = (y/10) | 0
+    //     }
+        
+    //     return x === reversed
+    // };
