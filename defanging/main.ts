5dcd17bb3ddaaf423274307e16ab2268d20b0be9
James Dinh <jdinh8124@gmail.com>
2020-11-11 11:58:07 -0800

    completed defanging ip address challenge

---
+++ defanging/main.ts
@@ -0,0 +1,13 @@
+function defangIPaddr(address: string): string {
+    const splitArr: string[]  = address.split('');
+    let returnedString: string = "";
+    for (let i = 0; i < splitArr.length; i++){
+        if (splitArr[i] === ".") {
+            returnedString += "[.]";
+        } else {
+            returnedString += splitArr[i];
+        }
+    }
+    
+    return returnedString
+};
