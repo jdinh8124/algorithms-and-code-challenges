ce0b2d7fbeb1a0bc37345077b6e7141b6b4e8ee2
James Dinh <jdinh8124@gmail.com>
2020-11-13 18:42:24 -0800

    finished code challenge with an alternative check

--- uniqueOccurrences/main.ts
+++ uniqueOccurrences/main.ts
@@ -1,13 +1,29 @@
 function uniqueOccurrences(arr: number[]): boolean {
-    const objToCheck = {}
+    let objToCheck : {[key: number]: number} = {}
 
     for (let i = 0; i <arr.length; i++) {
-        if (objToCheck[arr[i]]) {
+        if (objToCheck[arr[i]] === undefined) {
             objToCheck[arr[i]] = 1;
         } else {
             objToCheck[arr[i]] += 1;
         }
     }
-
+    const values = Object.values(objToCheck)
     
+    objToCheck = {}
+    for (let i = 0 ; i < values.length; i++) {
+        if (objToCheck[values[i]] === undefined) {
+            objToCheck[values[i]] = 1;
+        } else {
+            return false;
+        }    
+    }
+    
+    return true;
+
+//     const values = Object.values(objToCheck).sort((a,b) => a-b)
+//    for (let i = 0 ; i < values.length; i++) {
+//         if(values[i]===values[i+1]) return false;
+//     }
+//     return true;
 };
