ad449b8d0f22328eb016b1a318affa4e7bd54138
James Dinh <jdinh8124@gmail.com>
2020-11-14 18:44:24 -0800

    alternative solution created

--- robotMoves/main.ts
+++ robotMoves/main.ts
@@ -11,3 +11,28 @@ function judgeCircle(moves: string): boolean {
 
         return (objToCheck['U'] === objToCheck['D'] && objToCheck['L'] === objToCheck['R'])
 };
+
+
+// function judgeCircle(moves: string): boolean {
+//     let x = 0;
+//     let y = 0;
+//     for(let i = 0; i < moves.length; i++) {
+//         switch(moves[i]){
+//             case 'U':
+//                 y++;
+//                 break
+//             case 'D':
+//                 y--;
+//                 break
+//             case 'L':
+//                 x--;
+//                 break
+//             case 'R':
+//                 x++;
+//                 break;
+//         }
+//     }
+
+//     return x === 0 && y === 0
+// };
+
