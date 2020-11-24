a87967ac0df8e5552927aa564771127cf0c8ce94
James Dinh <jdinh8124@gmail.com>
2020-11-23 20:06:26 -0800

    finished code challenge with alternative solutions

--- countAndSay/main.ts
+++ countAndSay/main.ts
@@ -1,28 +1,42 @@
+// function isAnagram(s: string, t: string): boolean {
+//     if (s.length !== t.length) {
+//         return false
+//     }
+
+//     const objToCheck = {};
+
+//     for (let i = 0; i < s.length; i++) {
+//         if (objToCheck[s[i]] === undefined) {
+//             objToCheck[s[i]] = 1;
+//         } else {
+//             objToCheck[s[i]] = objToCheck[s[i]] + 1
+//         }
+
+//         if (objToCheck[t[i]] === undefined) {
+//             objToCheck[t[i]] = -1;
+//         } else {
+//             objToCheck[t[i]] = objToCheck[t[i]] - 1
+//         }
+//     }
+
+//     for (const prop in objToCheck) {
+//         if (objToCheck[prop] !== 0) {
+//             return false
+//         }
+//     }
+//     return true
+// };
+
+
+
+// function isAnagram(s: string, t: string): boolean {
+//     return s.split('').sort().join('') === t.split('').sort().join('');
+// };
+
 function isAnagram(s: string, t: string): boolean {
-    if (s.length !== t.length) {
-        return false
-    }
-
-    const objToCheck = {};
-
-    for (let i = 0; i < s.length; i++) {
-        if (objToCheck[s[i]] === undefined) {
-            objToCheck[s[i]] = 1;
-        } else {
-            objToCheck[s[i]] = objToCheck[s[i]] + 1
-        }
-
-        if (objToCheck[t[i]] === undefined) {
-            objToCheck[t[i]] = -1;
-        } else {
-            objToCheck[t[i]] = objToCheck[t[i]] - 1
-        }
-    }
-
-    for (const prop in objToCheck) {
-        if (objToCheck[prop] !== 0) {
-            return false
-        }
-    }
-    return true
+	const map = {};
+	s.split('').map(c => map[c] = map[c] ? map[c] + 1 : 1);
+	t.split('').map(c => map[c] = map[c] ? map[c] - 1 : -1);
+	return Object.keys(map).every(k => map[k] === 0)
 };
+
