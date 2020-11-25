69409153d5206569cd7cd1cfc9c21e9d0ea45a69
James Dinh <jdinh8124@gmail.com>
2020-11-24 19:09:00 -0800

    completed challenge using two pointer solution

---
+++ reverseString/main.ts
@@ -0,0 +1,11 @@
+// function reverseString(s: string[]): void {
+//     s.reverse();
+// };
+
+function reverseString(s: string[]): void {
+    for (let i = 0; i < s.length / 2; i++) {        
+        let temp = s[i];
+        s[i] = s[s.length - i - 1];
+        s[s.length - i - 1] = temp;
+    }
+};
