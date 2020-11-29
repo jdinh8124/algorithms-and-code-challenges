2e3e497b4a34a307ceac4bf4396675bbc381dc51
James Dinh <jdinh8124@gmail.com>
2020-11-28 18:17:53 -0800

    shorted code challenge to one line

--- isPowerOfThree/main.ts
+++ isPowerOfThree/main.ts
@@ -1,15 +1,15 @@
-function isPowerOfThree(n: number): boolean {
-    if (n >= 1) {
-        for (let i = 1; i <= n;) {
-            if (i === n) {
-                return true
-            }
-            i = i * 3
-        }
-    }
+// function isPowerOfThree(n: number): boolean {
+//     if (n >= 1) {
+//         for (let i = 1; i <= n;) {
+//             if (i === n) {
+//                 return true
+//             }
+//             i = i * 3
+//         }
+//     }
 
-    return false;
-};
+//     return false;
+// };
 
 function isPowerOfThree(n: number): boolean {
 return 0 < n && (3 ** 33) % n === 0
