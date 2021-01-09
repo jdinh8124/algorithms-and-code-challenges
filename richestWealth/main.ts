9e365bc03efb952862e4707cec546ac730e7a5d1
James Dinh <jdinh8124@gmail.com>
2021-01-09 11:19:58 -0800

    speed optimized for code challenge

--- richestWealth/main.ts
+++ richestWealth/main.ts
@@ -14,16 +14,28 @@
 //     return max;
 // };
 
-function maximumWealth(accounts: number[][]): number {
-    let max = 0;
+// function maximumWealth(accounts: number[][]): number {
+//     let max = 0;
     
-    accounts.forEach((element) => {
-        let count = element.reduce((total, current) => total + current)
+//     accounts.forEach((element) => {
+//         let count = element.reduce((total, current) => total + current)
         
-        if (count > max) {
-            max = count;
-        }
-    })
+//         if (count > max) {
+//             max = count;
+//         }
+//     })
     
-    return max;
+//     return max;
+// };
+
+// function maximumWealth(accounts: number[][]): number {
+//     return Math.max(...accounts.map((element) => {
+//         return element.reduce((total, current) => total + current)
+//     }))
+// };
+
+function maximumWealth(accounts: number[][]): number {
+    return accounts.map((element) => {
+        return element.reduce((total, current) => total + current)
+    }).sort((a,b) => b -a)[0]
 };
