d4b2fba2094cb8479fe83272ff8c23d7a689cd12
James Dinh <jdinh8124@gmail.com>
2021-01-09 12:15:15 -0800

    completed code challenge by using a map

--- groupAnagrams/main.js
+++ groupAnagrams/main.js
@@ -1,22 +1,32 @@
-function groupAnagrams(strs){    
-    const arrToReturn = [];
-    const objToCheck = {};
+// function groupAnagrams(strs){    
+//     const arrToReturn = [];
+//     const objToCheck = {};
 
-    strs.forEach((str) => {
-        const sorted = str.split('').sort().join('');
-        debugger
-        if (objToCheck[sorted]) {
-            objToCheck[sorted].push(str)
-        } else {
-            objToCheck[sorted] = [str];
+//     strs.forEach((str) => {
+//         const sorted = str.split('').sort().join('');
+//         if (objToCheck[sorted]) {
+//             objToCheck[sorted].push(str)
+//         } else {
+//             objToCheck[sorted] = [str];
+//         }
+//     })
+
+//     for (const prop in objToCheck) {
+//         arrToReturn.push(objToCheck[prop]);
+//     }
+
+//     return arrToReturn;
+// };
+
+
+let groupAnagrams = function(strs) {
+    let map = new Map();
+        for(let s of strs){
+            let res = s.split("").sort().join("");
+            if(!map.get(res)){
+                map.set(res, []);
+            } 
+            map.get(res).push(s);
         }
-    })
-
-    for (const prop in objToCheck) {
-        arrToReturn.push(objToCheck[prop]);
+        return Array.from(map.values());
     }
-
-    return arrToReturn;
-};
-
-console.log(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
