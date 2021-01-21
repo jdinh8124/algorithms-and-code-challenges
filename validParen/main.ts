0ff313ec56db610c0d645afa4595792ee03e4db1
James Dinh <jdinh8124@gmail.com>
2021-01-20 18:48:16 -0800

    code challenge completed

--- validParen/main.ts
+++ validParen/main.ts
@@ -1,3 +1,23 @@
 function isValid(s: string): boolean {
-
+    const stack = [s[0]];
+    
+    for (let i = 1; i < s.length; i++) {
+        if (stack.length) {
+            if (s[i] === ']' || s[i] === ')' || s[i] === '}') {
+                if (stack[stack.length - 1])
+            } else {
+                if (s[i] === '[' || s[i] === '(' || s[i] === '{') {
+                    stack.push(s[i])
+                } else {
+                    return false;
+                }
+            }
+        } else if (s[i] === '[' || s[i] === '(' || s[i] === '{') {
+            stack.push(s[i])
+        } else {
+            return false;
+        }
+    }
+        
+    return stack.length === 0;
 };
