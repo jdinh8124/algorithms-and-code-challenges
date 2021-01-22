f785cf804074d20a12d9ecdf47f9e0ab16bd3a27
James Dinh <jdinh8124@gmail.com>
2021-01-21 20:06:29 -0800

    completed code challenge

--- canPlaceFlowers/main.ts
+++ canPlaceFlowers/main.ts
@@ -1,3 +1,14 @@
-function canPlaceFlowers(flowerbed: number[], n: number): boolean {
-
-};
+function canPlaceFlowers(flowerbed: number[], n: number): boolean {    
+    for (let i = 0; i < flowerbed.length && n !== 0; i++) {
+          if (
+            flowerbed[i] === 0 &&
+            flowerbed[i - 1] !== 1 &&
+            flowerbed[i + 1] !== 1
+          ) {
+            n--;
+            i++;
+          }
+    }
+    return n === 0;
+    }
+    
